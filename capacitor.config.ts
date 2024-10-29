6a44b8047d6742989cd3e461acd1210f33ef551b
estefaniamsp <estefania.sanchez01@epn.edu.ec>
2024-10-28 23:28:47 -0500

    Cambios mejorados

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,24 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'io.ionic.starter',
   appName: 'prueba1',
-  webDir: 'www'
+  webDir: 'www',
+  bundledWebRuntime: false,
+  plugins: {
+    SplashScreen: {
+      launchShowDuration: 3000, 
+      launchAutoHide: true,
+      backgroundColor: "#ffffffff", 
+      abdroidScaleType: 'CENTER_CROP',
+      androidSplashResourceName: "splash",
+      showSpinner: false, 
+      androidSpinnerStyle: 'large',
+      iosSpinnerStyle: 'small',
+      spinnerColor: "#999999",
+      splashFullScreen: true,
+      splashImmersive: true,
+      layoutName: 'launch_screen'
+    }
+  }
 };
 
 export default config;
